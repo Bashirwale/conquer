42c9d06100a8cc0f4843ad0fd605e985c56f6d41
Abdullahi Bashir Wale <bashabdulbash@gmail.com>
2022-05-27 03:50:51 +0100

    Initial commit

---
+++ script.js
@@ -0,0 +1,30 @@
+  $(document).ready(function(){
+  $('#menuBar').click(function(){
+    $('#navigation').slideToggle(500);
+  });
+
+  $('#top, #navigation a').click(function(){
+
+ var userLocation = window.location.href;
+ var currentUrl = userLocation.substring(userLocation.lastIndexOf('#')+1);
+
+  if(currentUrl == 'home'){
+    $('html, body').animate({
+      scrollTop:$('#scroll-' + currentUrl).offset().top
+    },1000);
+  }else if(currentUrl == 'homepage'){
+    $('html, body').animate({
+      scrollTop: $('#scroll-' + currentUrl).offset().top
+    },1000);
+  }else if(currentUrl == 'aboutUs'){
+    $('html, body').animate({
+      scrollTop: $('#scroll-' + currentUrl).offset().top
+    },1000);
+  }
+  })
+  
+
+    
+
+  
+});
